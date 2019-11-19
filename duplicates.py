ca03048a74a8382fa22b23f74f2cb0d91c1dffa1
Rustymattok <you@example.com>
2019-11-19 20:41:53 +0300

    add new script

--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,49 @@
+import os
+import argparse
+
+
+def get_link_files(folder_path):
+    folder = []
+    for info_walk in os.walk(folder_path):
+        folder.append(info_walk)
+    return folder
+
+
+def optimize_memory(folder):
+    file_list_original = []
+    for address, dirs, files in folder:
+        for file in files:
+            if file in file_list_original:
+                file_path_remove = address+'/'+file
+                if os.path.isfile(file_path_remove):
+                    os.remove(file_path_remove)
+                    print('File: ', file_path_remove, ' was removed')
+                else:
+                    print('Error: %s file not found: ', file_path_remove)
+            else:
+                file_list_original.append(file)
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-f',
+        '--folder',
+        required=True,
+        help='command - folder for scan and clean'
+    )
+    return parser
+
+
+def main():
+    parser = create_parser()
+    args = parser.parse_args()
+    try:
+        folder_path = args.folder
+        optimize_memory(get_link_files(folder_path))
+    except ValueError:
+        print('not correct format')
+
 
 if __name__ == '__main__':
-    pass
+    main()
